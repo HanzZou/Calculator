baf97df68c35a11be9e0e91c97d7cfd7b7be1ada
HanzZou <704519032@qq.com>
2016-07-14 17:43:53 +0800

    完成第一讲，初步创建键盘和显示区。

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Hanz on 16/7/13.
+//  Copyright © 2016年 Hanz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textInCurrentDisplay = display.text!
+            display.text = textInCurrentDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+    
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+            
+        }
+    }
+}
+
